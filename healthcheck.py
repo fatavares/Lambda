d7929f3581cb2c0995b62b4d7024be537c3e2b4a
Felipe Tavares <felipe.tavares@centurylink.com>
2018-09-02 15:08:00 -0300

    Descriptions

--- healthcheck.py
+++ healthcheck.py
@@ -1,7 +1,16 @@
+"""
+Lambda Function build to create a Health Check of a Service on a specific server.
+If you want to create a Failover DNS Record for a private service you can use the CloudWatch metric to create a
+Route 53 health check from the metric ALARM
+The Route 53 Endpoint Health Check needs the subnet to be public.
+This Lambda Function requires a IAM Role with permission AWSLambdaENIManagementAccess and CloudWatchFullAccess, the
+Lambda Function must be configured on a VPC, inside a private subnet that have a NAT Gateway
+"""
+
 from socket import socket
 import boto3
 
-
+# Open a socket on a specified TCP port
 def tcp_test(server, port):
     try:
         sock = socket()
@@ -12,15 +21,13 @@ def tcp_test(server, port):
         return False
 
 # Create CloudWatch client
-
-
 cloudwatch = boto3.client('cloudwatch')
 
 
+# Put the result in a custom Cloudwatch metric
 def put_data(ip, port):
-    if tcp_test(ip, port) == True:
+    if tcp_test(ip, port) is True:
         # Put custom metrics
-        print("OK")
         cloudwatch.put_metric_data(
             MetricData=[
                 {
@@ -38,7 +45,6 @@ def put_data(ip, port):
             Namespace='LambdaHealthCheck'
         )
     else:
-        print("FALHA")
         cloudwatch.put_metric_data(
             MetricData=[
                 {
@@ -55,6 +61,7 @@ def put_data(ip, port):
             ],
             Namespace='LambdaHealthCheck'
         )
+
 
 def lambda_handler(event, context):
     put_data('192.168.0.1', 80)
